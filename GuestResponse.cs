161ca7e51b5d68457cf30ab0f43353e9d92673c2
Gearron <81452792+Gearron@users.noreply.github.com>
2025-05-15 17:01:32 -0500

    Add files via upload

---
+++ GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+
+        [Required(ErrorMessage = "Please enter your name")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public required string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+"# party-invite-web-app" 
+"# party-invite-web-app" 
+"# party-invite-web-app" 
+"# party-invite-web-app" 
